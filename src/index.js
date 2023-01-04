7bb49cd78fc860606899561b9cd118bd61ce454b
F3nto <pyaesonetun1141999@gmail.com>
2023-01-04 10:56:29 +0630

    cookie and session

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 const groceriesRoute = require('./routes/groceries')
-const marketRouter = require('./routes/markets')
+const marketRoute = require('./routes/markets')
 
 require('dotenv').config()
 
 const app = express()
 
 app.use(express.json())
-
 app.use(express.urlencoded())
+
+
+
+app.use(cookieParser())
+app.use(session({ secret : "FHONFNOWNFWHOF02IOEIF2JIO23RU1344##$%", resave:false, saveUninitialized:false}))
+
+
 
 app.use((req, res, next) => {
     
@@ -19,7 +27,7 @@ app.use((req, res, next) => {
 })
  
 app.use('/api/v1/grocery',groceriesRoute)
-app.use('/api/v1/market', marketRouter)
+app.use('/api/v1/market', marketRoute)
 
 
 app.listen(process.env.PORT, () => {console.log(`Server Running ${process.env.PORT}!!!`)})
--- src/routes/groceries.js
+++ src/routes/groceries.js
@@ -2,8 +2,6 @@ const {Router} = require('express')
 
 const router = Router();
 
-
-
 let groceryList = [
 
     {
@@ -33,13 +31,17 @@ let groceryList = [
 
 router.get('/' , (req, res) => {
 
-    res.send(groceryList)
+    
+    res.cookie('visited', true, {maxAge:10000})
+    res.send(groceryList);
 
-  
+   
 
 })
 
 router.get('/:item', (req, res) => {
+
+    console.log(req.cookies)
 
     const {item} = req.params
 
@@ -60,6 +62,53 @@ router.post('/', (req, res) => {
     res.send(201) 
 
     console.log(req.body)
+})
+
+
+router.get('/shopping/cart', (req, res) => {
+
+    const {cart} = req.session
+
+    if(!cart){
+
+        res.send("You have no cart session!!!")
+
+    }else{
+
+        res.send(cart)
+
+    }
+
+
+})
+
+router.post('/shopping/cart/item', (req, res) => {
+
+    const {item , quantity} = req.body
+
+    const cartItem = {item, quantity}
+
+    const {cart} = req.session
+
+    if(cart) {
+
+        cart.items.push(cartItem)
+ 
+    }else{
+
+        req.session.cart = {
+
+            items : [cartItem]
+
+        }
+
+
+    }
+
+    res.send(201)
+
+    console.log(cartItem)
+
 })
 
 module.exports = router;
